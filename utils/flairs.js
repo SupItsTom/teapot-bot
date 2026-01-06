85768a8dd867b38e903f28894330d13a98ba5e82
SupItsTom <github@supitstom.net>
2026-01-06 21:45:33 +0000

    feat: flairs to include hyperlink to docs

--- utils/flairs.js
+++ utils/flairs.js
@@ -7,7 +7,7 @@ export class Flairs {
 
   GetFlair(key) {
     // Dec 11th: Using text-based fallback as new icons haven't been uploaded yet.
-    return `\`ⓘ ${key}\``;
+    return `[\`ⓘ ${key}\`](https://supitstom.net/teapot-bot/badges/#flairs)`;
     
     if (this.env.DISCORD_APPLICATION.CLIENT_ID !== "1040809994012069919") {
       return `\`ⓘ ${key}\``;
