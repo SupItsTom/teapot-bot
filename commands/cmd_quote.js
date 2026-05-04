f3f5f17c3919bddc00267bd491cd7cd3622998a0
SupItsTom <github@supitstom.net>
2026-05-04 18:36:37 +0100

    style: update quote format

--- commands/cmd_quote.js
+++ commands/cmd_quote.js
@@ -9,7 +9,21 @@ import { ButtonStyle, ComponentType } from "discord-api-types/v10";
  * Retrieve a random quote from SkidPaste
  */
 export default async function (interaction, env, ctx) {
-  const quote = await postTeapotRequest(env, { action: "randquote" });
+  // decide which we wanna use:
+  let quote_type = 0;
+
+  switch(quote_type){
+    case 0: return cmd_quote_skidpaste(interaction, env, ctx);
+    case 1: return cmd_quote_discord(interaction, env, ctx);
+  }
+}
+
+export async function cmd_quote_discord(interaction, env, ctx){
+
+}
+
+export async function cmd_quote_skidpaste(interaction, env, ctx){
+const skidpaste = await postTeapotRequest(env, { action: "randquote" });
 
   return new JsonResponse({
     type: InteractionResponseType.CHANNEL_MESSAGE_WITH_SOURCE,
@@ -20,11 +34,9 @@ export default async function (interaction, env, ctx) {
         {
           type: MessageComponentTypes.CONTAINER,
           components: [
-            MessageComponent.Text(`**QUOTE**`, -1),
-            MessageComponent.Text(`${quote.name}`, 2),
-            MessageComponent.Text(`Posted to **SkidPaste** <t:${quote.date}:R>${quote.name !== "Unnamed" ? ` by **${quote.name}**` : ""}.`, -1),
-            MessageComponent.Seperator(),
-            MessageComponent.Text(`${quote.quote}`, 0),
+            MessageComponent.Text(`**A WILD QUOTE APPEARS...**`, -1),
+            MessageComponent.Text(`>>> ${skidpaste.quote}`, 3),
+            MessageComponent.Text(`<:TeapotPlatform:1500906872822038669> ${skidpaste.name !== "Unnamed" ? `**${skidpaste.name}**` : "**SkidPaste User**"} • <t:${skidpaste.date}:R>`, 0),
           ]
         }
       ]
