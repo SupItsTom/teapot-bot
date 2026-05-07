bda6dd527c9322eb9fe16cc0aa56dfbccfe69879
SupItsTom <github@supitstom.net>
2026-05-07 21:03:45 +0100

    fix: add temporary platform icons

--- commands/cmd_profile.js
+++ commands/cmd_profile.js
@@ -88,7 +88,7 @@ export default async function (interaction, env, ctx) {
             MessageComponent.Seperator(),
             MessageComponent.Text(`
 -# **MEMBER SINCE**
--# \`XBLS\` <t:${teapot.user.date_registered_unix}:D> **•** \`BOT\` <t:${Math.floor(new Date(bot_user.timestamp) / 1000)}:D>
+-# <:Teapot:1500914280051114064> <t:${teapot.user.date_registered_unix}:D> **•** <:Discord:1502038062115328301> <t:${Math.floor(new Date(bot_user.timestamp) / 1000)}:D>
 `),
           ]
         },
